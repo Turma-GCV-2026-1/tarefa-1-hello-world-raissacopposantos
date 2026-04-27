63610ecefa867c8abe1b143df3e0c8466993b235
Raíssa Coppo Santos <raissa.coppo.santos@gmail.com>
2026-04-27 11:15:14 +0000

    Adicionando nova funcionalidade

--- main.c
+++ main.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    printf("Atualização Professor!\n")
     printf("Hello, World!\n");
-    printf("Aluno editando. v2\n");
-    printf("Aluno editando. v3\n");
+    printf("Aprendendo Sobre Branch\n");
     return 0;
 }
